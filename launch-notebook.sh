741e83b0bfeea140f797aa8e926f79b16634f515
Jack S. Hale <mail@jackhale.co.uk>
2021-03-11 09:08:36 +0100

    Test showing issue in current DOLFINx.

--- launch-notebook.sh
+++ launch-notebook.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 CONTAINER_ENGINE="docker"
-${CONTAINER_ENGINE} run -ti -v $(pwd):/shared -w /shared -p 8888:8888 --init dolfinx/lab
+docker pull dolfinx/lab
+${CONTAINER_ENGINE} run -ti -v $(pwd):/shared -w /shared --publish-all --init dolfinx/lab
