021dc96b8a11a06f6f8ea6f58ea5096821aa51d2
Jack S. Hale <mail@jackhale.co.uk>
2021-04-13 16:51:16 +0200

    Still broken.

--- demo/documented/reissner-mindlin-clamped/test_mini.py
+++ demo/documented/reissner-mindlin-clamped/test_mini.py
@@ -5,27 +5,23 @@ from mpi4py import MPI
 from petsc4py import PETSc
 
 import dolfinx
-from dolfinx import UnitSquareMesh, Function, FunctionSpace
+from dolfinx import UnitSquareMesh, FunctionSpace
 from dolfinx.cpp.mesh import CellType
-from dolfinx.fem import assemble_matrix, assemble_vector
+from dolfinx.fem import assemble_matrix
 
 import ufl
 from ufl import FiniteElement, MixedElement
-from ufl import dx, inner, split
+from ufl import dx, inner
 
 mesh = UnitSquareMesh(MPI.COMM_WORLD, 1, 1, CellType.triangle,
                       dolfinx.cpp.mesh.GhostMode.shared_facet)
 
-#U_el = MixedElement([FiniteElement("N1curl", ufl.triangle, 1), FiniteElement("N1curl", ufl.triangle, 1)])
 #U_el = MixedElement([FiniteElement("N1curl", ufl.triangle, 1)])
 U_el = FiniteElement("N1curl", ufl.triangle, 1)
 U = FunctionSpace(mesh, U_el)
 
 u = ufl.TrialFunction(U)
 v = ufl.TestFunction(U)
-
-#u, _ = split(u)
-#v, _ = split(v)
 
 print(u)
 
