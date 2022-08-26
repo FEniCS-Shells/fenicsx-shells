8cee173acfdb4d66655e1d4ae6e280950f6fa286
Jack S. Hale <mail@jackhale.co.uk>
2022-08-26 14:01:22 +0200

    Tidy up

--- build-image.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker buildx build --file Dockerfile.local -t jhale/fenics-shellsx:latest .
--- launch-notebook.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-CONTAINER_ENGINE="docker"
-${CONTAINER_ENGINE} run --rm -ti -v $(pwd):/shared -w /shared --publish-all --init jhale/fenics-shellsx:latest
