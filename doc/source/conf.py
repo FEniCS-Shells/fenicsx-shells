702e0c24d1c2f284a29457d1c2266e902ebd0056
Jack S. Hale <mail@jackhale.co.uk>
2024-03-26 16:24:32 +0100

    Updates for development (#11)
    * Update to dev.
    
    * More fixes for dev.
    
    * Fix and switch to ruff.
    
    * Ruff format.
    
    * More ruff fixes.
    
    * Remove C++ - can add back in as it will need to be totally rethought.
    
    * Fix.
    
    * Remove setup.cfg

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -9,42 +9,51 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath("."))
 import jupytext_process
 
 jupytext_process.process()
 
-project = 'FEniCSx-Shells'
-copyright = '2022, FEniCSx-Shells Authors'
-author = 'FEniCSx-Shells Authors'
-release = '0.5.0.dev0'
+project = "FEniCSx-Shells"
+copyright = "2022, FEniCSx-Shells Authors"
+author = "FEniCSx-Shells Authors"
+release = "0.5.0.dev0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc',
-              'sphinx.ext.autosummary',
-              'sphinx.ext.mathjax',
-              'sphinx.ext.napoleon',
-              'sphinx.ext.todo',
-              'sphinx.ext.viewcode',
-              'myst_parser', ]
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
+    "myst_parser",
+]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
-source_suffix = ['.rst', '.md']
+source_suffix = [".rst", ".md"]
 
 html_theme = "sphinx_rtd_theme"
 
-myst_enable_extensions = ["dollarmath",]
+myst_enable_extensions = [
+    "dollarmath",
+]
 
-autodoc_default_options = {'members': True, 'show-inheritance': True, 'imported-members': True, 'undoc-members': True}
+autodoc_default_options = {
+    "members": True,
+    "show-inheritance": True,
+    "imported-members": True,
+    "undoc-members": True,
+}
 autosummary_generate = True
 autoclass_content = "both"
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
