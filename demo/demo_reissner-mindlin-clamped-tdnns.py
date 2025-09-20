73ee3880ba309c623689cd4acf633b9f8117e67f
Jack S. Hale <mail@jackhale.co.uk>
2025-09-20 15:04:11 +0200

    Update demo_reissner-mindlin-clamped-tdnns.py

--- demo/demo_reissner-mindlin-clamped-tdnns.py
+++ demo/demo_reissner-mindlin-clamped-tdnns.py
@@ -243,6 +243,7 @@ problem = LinearProblem(
     a,
     L,
     bcs=bcs,
+    petsc_options_prefix="rm",
     petsc_options={"ksp_type": "preonly", "pc_type": "lu", "pc_factor_mat_solver_type": "mumps"},
 )
 u_h = problem.solve()
